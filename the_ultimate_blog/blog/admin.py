ddc12adc74112a45fe54078069de5e190f511b00
mohamed Ali <solutiond700@gmail.com>
2022-12-16 14:58:17 +0200

    adding blog models to admin

--- the_ultimate_blog/blog/admin.py
+++ the_ultimate_blog/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import *
-
+from .models import Category, Article, Comment
 
 admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Comment)
